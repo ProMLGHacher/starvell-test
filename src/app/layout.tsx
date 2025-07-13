085126d008d8d982cae4c38046f3f953ebe7c45c
Araik Gukasyan <itismymessagebox@mail.ru>
2025-07-13 15:06:41 +0500

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,31 @@ export const metadata: Metadata = {
   robots: {
     follow: false,
   },
+  openGraph: {
+    title: "STARVELL",
+    description:
+      "На бирже STARVELL вы можете купить игровую валюту, предметы, услуги и другие игровые ценности напрямую у других игроков, а также продать свои.",
+    images: [
+      {
+        url: "/og-image.png",
+      },
+    ],
+    type: "website",
+    siteName: "STARVELL",
+    url: "https://starvell.com",
+    locale: "ru_RU",
+    countryName: "Россия",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "STARVELL",
+    creator: "araik.dev",
+    images: [
+      {
+        url: "/og.png",
+      },
+    ],
+  },
   description:
     "На бирже STARVELL вы можете купить игровую валюту, предметы, услуги и другие игровые ценности напрямую у других игроков, а также продать свои.",
 };
